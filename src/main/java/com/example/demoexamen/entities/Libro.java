733bb0308b6bbc8163356f97648f6b9218774429
lvarezisabel7 <maisabel2000@gmail.com>
2024-03-04 09:53:54 +0100

    Start project

---
+++ src/main/java/com/example/demoexamen/entities/Libro.java
@@ -0,0 +1,41 @@
+package com.example.demoexamen.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "libros")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Libro implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "The title cannot be empty")
+    private String titulo;
+
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private LocalDate date;
+
+}
