451258ad67fe5f08b44d5461a0e99a9690e428e2
lvarezisabel7 <maisabel2000@gmail.com>
2024-03-04 10:35:22 +0100

    Add LibroService, LibroServiceImpl y metodo en LibroDao

--- src/main/java/com/example/demoexamen/dao/LibroDao.java
+++ src/main/java/com/example/demoexamen/dao/LibroDao.java
@@ -3,8 +3,11 @@ package com.example.demoexamen.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demoexamen.entities.Libro;
+import java.util.List;
+
 
 public interface LibroDao extends JpaRepository<Libro, Integer> {
 
+    Libro findById(int id);
 
 }
