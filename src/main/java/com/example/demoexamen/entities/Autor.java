2d4e951a3e40b556699e431cfc06f324511f9a41
lvarezisabel7 <maisabel2000@gmail.com>
2024-03-04 10:10:57 +0100

    Add entity Autor

--- src/main/java/com/example/demoexamen/entities/Autor.java
+++ src/main/java/com/example/demoexamen/entities/Autor.java
@@ -1,9 +1,19 @@
 package com.example.demoexamen.entities;
 
 import java.io.Serializable;
+import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,11 +24,27 @@ import lombok.Setter;
 @Getter
 @Setter
 @Table(name = "autores")
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Autor implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "The name cannot be empty")
+    private String nombre;
+
+     @ManyToMany(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+        }, 
+        mappedBy = "autores")
+    @JsonIgnore
+    private Set<Libro> libros;
+
 
 }
